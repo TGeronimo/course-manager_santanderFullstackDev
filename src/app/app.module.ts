38326104f8234c3306452ffb9edbd688df388f98
TGeronimo <thi.matheus@gmail.com>
2021-08-22 19:32:11 -0300

    Inserted data into courses component
    > initial pipe concepts application
    > added star-rating but not visually working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from "./star/star.component";
+import { ReplacePipe } from "./pipe/replace.pipe";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,41 +1,36 @@
 import { Component } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 import { OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-course-list',
-  templateUrl: './course-list.component.html',
+  templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit {
 
-  courses: Course[] = [];
+  filteredCourses: Course[] = [];
+  _courses: Course[] = [];
+
+  _filterBy: string;
+
+  constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '',
-        price: 99.99,
-        code: 'XPS-8796',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: 'December, 2, 2019'
-      },
-      {
-        id: 1,
-        name: 'Angular: HTTP',
-        imageUrl: '',
-        price: 45.99,
-        code: 'LKL-1094',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'December, 4, 2019'
-      }
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
 
-    ]
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+  }
+
+  get filter() {
+    return this._filterBy;
   }
 
 }
